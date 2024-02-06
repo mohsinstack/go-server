3b68cb41d3f246bb872248224f903c96a25a75a5
Mohsin Khan <faiz.aks@gmail.com>
2024-02-07 02:32:49 +0530

    Working Copy

--- routes/routes.go
+++ routes/routes.go
@@ -2,15 +2,30 @@ package routes
 
 import (
 	"go-server/controllers"
+	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // Client Origin - React Local
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * 60 * 60,
+	}))
+
 	// All routes
 	r.GET("/users", controllers.GetUsers)
 	r.POST("/users", controllers.CreateUser)
 	r.GET("/user/:id", controllers.GetUserByID)
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "CORS should now be handled by middleware"})
+	})
 	return r
 }
